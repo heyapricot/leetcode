ecb9092b09ffb79227659e519c02c3542ecc6d3f
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2019-02-04 14:37:13 -0600

    Solved Find Min rotated

---
+++ javascript/findMinRotated.js
@@ -0,0 +1,11 @@
+const findMin = (nums) => {
+  let i = 0;
+  let runner = 0;
+  for(let i = 0; i < nums.length; i += 1){
+    runner = i + 1;
+    if (nums[runner] < nums[i] && runner !== nums.length) return nums[runner]
+  }
+  return nums[0]
+};
+
+console.log(findMin([3,4,5,1,2]));
