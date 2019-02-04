1e323f8705157ff9ee95948b5ef348484c302cb2
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2019-02-04 13:05:45 -0600

    Removed logs

--- javascript/rotateArray.js
+++ javascript/rotateArray.js
@@ -1,10 +1,6 @@
 const rotate = (nums, k) => {
   for(let i = 0; i < k; i += 1){
     nums.splice(0,0,nums.pop());
-    console.log(nums)
   }
   return nums;
 };
-
-
-console.log(rotate([1,2,3,4,5,6,7], 3));
