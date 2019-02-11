cba3e37b4932655e68f819196072bd549d016dbb
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2019-02-11 09:58:44 -0600

    Recursive roman passed

--- javascript/roman.js
+++ javascript/roman.js
@@ -1,5 +1,7 @@
 const roman = (input, index = 0) => {
-  const equivalence = { I: 1, V: 5, X: 10 };
+  const equivalence = {
+    I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000,
+  };
   const currentChar = input[index];
   let currentValue = equivalence[currentChar];
   let nextValue = 0;
@@ -8,10 +10,14 @@ const roman = (input, index = 0) => {
   switch (currentChar) {
     case 'I':
     case 'X':
+    case 'C':
+    case 'M':
       if (nextValue < 3 * currentValue) currentValue += nextValue;
       else currentValue = nextValue - currentValue;
       break;
     case 'V':
+    case 'L':
+    case 'D':
       if (nextValue < currentValue) currentValue += nextValue;
       break;
   }
@@ -19,4 +25,4 @@ const roman = (input, index = 0) => {
   return currentValue;
 };
 
-console.log(roman('IIII'));
+console.log(roman('MII'));
