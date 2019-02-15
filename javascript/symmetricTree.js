611f319807226f1095af65ae5b3e534423925ffd
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2019-02-15 06:38:25 -0600

    Solved symemtric tree

--- javascript/symmetricTree.js
+++ javascript/symmetricTree.js
@@ -1,36 +1,12 @@
-const TreeNode = (val) => {
-  const left = null;
-  const right = null;
-  return { left, right, val };
-};
-
 const isSymmetric = (root) => {
-  const BFS = (node, queue = [], output = []) => {
-    if (node !== null) {
-      // Visit node
-      output.push(node.val);
+  if (!root) return true;
 
-      // Add children to the queue
-      [node.left, node.right].forEach((nod) => { queue.push(nod); });
-    } else {
-      // node is null
-      output.push(null);
-    }
 
-    if (queue.length > 0) output = BFS(queue.splice(0, 1)[0], queue, output);
-    return output;
+  const isMirror = (left, right) => {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
   };
 
-  return BFS(root);
+  return isMirror(root.left, root.right);
 };
-
-
-const root = TreeNode(1);
-root.left = TreeNode(2);
-root.left.left = TreeNode(3);
-// root.left.right = TreeNode(4);
-root.right = TreeNode(3);
-root.right.left = TreeNode(4);
-root.right.right = TreeNode(3);
-
-console.log(isSymmetric(root));
